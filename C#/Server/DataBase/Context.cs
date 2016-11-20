314c2bf916cb3dd5e13abdfc4cd3a7c77f1bc8f9
RanKor <randomkorrektor@gmail.com>
2016-11-20 16:21:10 +0700

    MogoDb Progress

--- C#/Server/DataBase/Context.cs
+++ C#/Server/DataBase/Context.cs
@@ -1,6 +1,9 @@
 ﻿using DataTypes;
+using MongoDB.Driver;
+using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -10,14 +13,32 @@ namespace DataBase
 {
     public class Context : DbContext
     {
-        public static Context Instance = new Context();
-        public DbSet<Comment> comments { get; set; }
-        public DbSet<Like> likes { get; set; }
-        public DbSet<Message> messages { get; set; }
-        public DbSet<Post> posts { get; set; }
-        public DbSet<User> users { get; set; }
+        public static Context Instanse = new Context();
+        private MongoClient client;
+        private IMongoDatabase database;
 
-        private Context(): base("name=ServerDB")
-        { }
+        private Context()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ServerDB"].ConnectionString;
+            client = new MongoClient(connectionString);
+            database = client.GetDatabase(ConfigurationManager.AppSettings["DBName"]);
+        }
+
+        public void DropDB ()
+        {
+            database.Client.DropDatabase(ConfigurationManager.AppSettings["DBName"]);
+        }
+
+
+
+
+
+
+
+        //public DbSet<Comment> comments { get; set; }
+        //public DbSet<Like> likes { get; set; }
+        //public DbSet<Message> messages { get; set; }
+        //public DbSet<Post> posts { get; set; }
+        //public DbSet<User> users { get; set; }
     }
 }
